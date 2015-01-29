9df75ecd7b3004e777cfa12ddc1b78401ffb488c
Eric Tsang <surplus.et@gmail.com>
2015-01-28 23:54:33 -0800

    minor changes to Application, and templete
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -28,7 +28,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">{title}</a>
+                <a class="navbar-brand" href="{baseurl}">{title}</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 {navbar}
