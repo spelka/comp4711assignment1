c981de4cd7f03c31c80b3bff084b0d3b00964c86
3rafanan <3rafanan@gmail.com>
2015-02-18 14:55:23 -0800

    Updated User_detail
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/controllers/User_detail.php
+++ application/controllers/User_detail.php
@@ -56,7 +56,58 @@ class User_detail extends Application {
 	 */
 	public function index()
 	{
-		//$this->load->view('profile_management');
+		        // generate cards
+        $this->load->model('ads');
+        $ads = $this->ads->all();
+
+        $cards = array();
+
+        foreach($ads as $ad)
+        {
+            $card = array();
+            $card['cardlink']       = base_url('Ad_Detail');
+            $card['cardimgsrc']     = base_url('assets/img/portfolio/cabin.png');
+            $card['cardimagealt']   = $ad['title'];
+            $card['cardtitle']      = $ad['title'];
+            $card['cardcaption']    = $ad['description'];
+            $cards[] = $this->parser->parse('_card',$card,true);
+        }
+
+        // generate columns, with the cards inside them
+        $columns = array();
+        while(count($cards) > 0)
+        {
+            $column = array();
+            $column['columnclass']  = 'col-sm-4';
+            $column['content']      = array_pop($cards);
+            $columns[] = $column;
+        }
+
+        // generate rows with the columns inside them (3 columns per row)
+        $rows = array();
+        $rows['row'] = array();
+        while(count($columns) > 0)
+        {
+            $row = array();
+            $row['column'] = array();
+            for($count = 0; $count < 3; $count++)
+            {
+                if(count($columns) > 0)
+                {
+                    $row['column'][] = array_pop($columns);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            $rows['row'][] = $row;
+        }
+
+        // generate the grid
+        // $this->data['cards'] = $cards[0];
+        $this->data['cards'] = $this->parser->parse('_grid', $rows, true);
+
 
         $this->data['pagetitle'] = 'User Detail';
         $this->data['pagebody'] = 'user_detail';
