0d6eb04d73317523d0b6b890fac62df6fd43598a
lamckalex <lam.ck.alex@gmail.com>
2015-03-03 18:53:41 -0800

    Admin page now shows all user and post data
    Signed-off-by: lamckalex <lam.ck.alex@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,38 +1,59 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-/**
-*	Based on instructions found on the CodeIgniter website:
-*	https://ellislab.com/codeigniter/user-guide/libraries/form_validation.html#theform
-*/
-class Admin extends Application {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *         http://example.com/index.php/welcome
-     *    - or -
-     *         http://example.com/index.php/welcome/index
-     *    - or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see http://codeigniter.com/user_guide/general/urls.html
-     */
+class Admin extends Application {
 
 	 public function index()
 	 {
+				$this->load->model('users');
+				$this->load->model('ads');
+				$this->load->model('categories');
+
         $this->data['page_title'] = '';
         $this->data['page_body'] = 'admin.php';
+				$this->data['adlist'] = $this->ads->all();
 
-///////////////////////////////////////////////////////////////////////////////
+
+
+				$this->data['userlist'] = $this->users->all();
+				/*
 
         $this->load->helper('html');
 
-///////////////////////////////////////////////////////////////////////////////
+				$ads = $this->ads->all();
+
+				$adDataArray = array();
+
+				foreach($ads as $ad)
+				{
+						$adData = array();
+
+						$adData['ID'] 				= $ad->ID;
+						$adData['userID']     = $ad->userID;
+						$adData['uploaded']   = $ad->uploaded;
+						$adData['price']      = $ad->price;
+						$adData['flags']    	= $ad->flags;
+						$adData['description']= $ad->description;
+						$adData['categoryID'] = $ad->categoryID;
+						$adData['title']			= $ad->title;
+
+						array_push($adDataArray, $adData);
+				}
+
+				$adList = array();
+				foreach ($adDataArray as $adData)
+				{
+						$adList[] = anchor('', $adData['ID'], 'title="'.$adData['ID'].'"');
+				}
+
+				$attributes = array();
+				$attributes['class'] = 'list-group';
+				$attributes['id']    = 'adList';
+
+				$this->data['adlist'] = ul($adList, $attributes);
+*/
+/*
         $adDataArray = array(
             array(
                 'ID'                => 1,
@@ -60,20 +81,9 @@ class Admin extends Application {
                 'name'              => 'Other')
         );
 
-        $adList = array();
-        foreach ($adDataArray as $adData)
-        {
-            $adList[] = anchor(
-                '', $adData['name'], 'title="'.$adData['name'].'"');
-        }
 
-        $attributes = array();
-        $attributes['class'] = 'list-group';
-        $attributes['id']    = 'adList';
 
-        $this->data['adlist'] = ul($adList, $attributes);
 
-///////////////////////////////////////////////////////////////////////////////
 
         $userDataArray = array(
             array(
@@ -114,8 +124,7 @@ class Admin extends Application {
         $attributes['id']    = 'userList';
 
         $this->data['userlist'] = ul($userList, $attributes);
-
-///////////////////////////////////////////////////////////////////////////////
+				*/
         $this->render();
 	 }
 }
