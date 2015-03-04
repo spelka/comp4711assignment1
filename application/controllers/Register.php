1154c5b8e1e1ad701d2e638410797016aa5e9bbb
3rafanan <3rafanan@gmail.com>
2015-03-04 12:23:04 -0800

    Register now adds a record into users database
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -43,6 +43,86 @@ class Register extends Application {
     {
         parent::__construct();
         $this->load->helper('formfields');
+        $this->load->model('users');
+    }
+
+    public function confirm()//$record)
+    {
+        //if(empty($record))
+        $record = new stdClass();
+
+        // Extract submitted fields
+        $record->fdisplayname = $this->input->post('dname');
+        $record->fusername = $this->input->post('uname');
+        $record->fpassword = $this->input->post('pswd');
+        $record->fcpassword = $this->input->post('cpswd');
+        $record->femail = $this->input->post('email');
+
+        // validation
+        if(empty($record->fdisplayname))
+            $this->errors[] = 'You must specify display name.';
+        if(empty($record->fusername))
+            $this->errors[] = 'You must specify user name.';
+        // add database checking for username
+
+        if(empty($record->fpassword))
+            $this->errors[] = 'No password set.';
+        if(empty($record->fcpassword))
+            $this->errors[] = 'Confirm password.';
+
+        if(strcmp($record->fpassword, $record->fcpassword) !== 0)
+            $this->errors[] = 'Password do not match';
+
+
+        if (!filter_var($record->femail, FILTER_VALIDATE_EMAIL))
+            $this->errors[] = "Invalid email format.";
+
+        // redisplay if any errors
+        if(count($this->errors) > 0)
+        {
+            $this->display_error($record);
+            return; // make sure we don't try to save anything
+        }
+
+        // create a record to add to the database
+        $addRecord = $this->users->create();
+
+        $addRecord->ID = $this->users->highest() + 1;
+        $addRecord->type = 0;
+        $addRecord->username = $record->fusername;
+        $addRecord->password = $record->fpassword;
+        $addRecord->email = $record->femail;
+        $addRecord->displayname = $record->fdisplayname;
+
+        $this->users->add($addRecord);
+
+        redirect('/welcome');
+    }
+
+    public function display_error($record)
+    {
+        // format any errors
+        $message = '';
+
+        if(count($this->errors) > 0)
+        {
+            foreach($this->errors as $error)
+                $message .= $error . BR;
+        }
+
+        $this->data['message'] = $message;
+
+        $this->data['fdisplayname'] = makeTextField('Display Name:', 'dname', $record->fdisplayname);
+        $this->data['fusername'] = makePasswordField('User Name:', 'uname', $record->fusername);
+        $this->data['fpassword'] = makePasswordField('Password', 'pswd', $record->fpassword);
+        $this->data['fcpassword'] = makePasswordField('Confirm Password', 'cpswd', $record->fcpassword);
+        $this->data['femail'] = makeTextField('Email', 'email', $record->femail);
+        $this->data['fsubmit'] = makeSubmitButton('Submit', 'Submit');
+        $this->data['fcancel'] = makeCancelButton('Cancel');
+
+        $this->data['page_body'] = 'register';
+
+        $this->render();
     }
 
 	public function index()
@@ -50,6 +130,7 @@ class Register extends Application {
         $this->data['page_title'] = 'Register';
         $this->data['page_body'] = 'register';
         $this->data['navbar_activelink'] = base_url('/Register');
+        $this->data['message'] = '';
 
         // Create form fields
         $this->data['fdisplayname'] = makeTextField('Display Name:', 'dname', '');
--- application/views/register.php
+++ application/views/register.php
@@ -1,6 +1,7 @@
 <div class="container">
     <h2>Register</h2>
-    <form action="" method="post">
+    <div class="errors">{message}</div>
+    <form action="/register/confirm" method="post">
         {fdisplayname}
         {fusername}
         {fpassword}
