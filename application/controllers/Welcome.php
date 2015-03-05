caacf3b171d8714c9c6b36a1c73f84089da00a53
3rafanan <3rafanan@gmail.com>
2015-03-05 13:35:29 -0800

    Fixed merging errors in welcome page
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -28,43 +28,12 @@ class Welcome extends Application {
 
         $ads = $this->ads->all();
 
-<<<<<<< HEAD
-        // generate the grid
         $grid = generateCards($this, $ads);
         $this->data['cards'] = $this->parser->parse('_grid', $grid, true);
-=======
-        $this->data['cards'] = $this->generateCards($ads);
+
         $this->data['categories'] = $this->generateCategories();
 
         $this->render();
-    }
-
-    public function generateCards($ads)
-    {
-      ////////////////////
-      // generate cards //
-      ////////////////////
-
-      // putting ads onto the card views
-      $cards = array();
-      foreach($ads as $ad)
-      {
-          $card = adToCard($this, $ad);   // convert ad into a card object
-          $cards[] = $this->parser->parse('_card', $card, true);
-      }
-
-      // put cards into columns
-      $columns = makeColumns('col-sm-4', $cards);
-
-      // generate rows with the columns inside them (3 columns per row)
-      $grid = array();
-      $grid['rows'] = makeGroups(3, 'columns', $columns);
->>>>>>> 2a93a890a69caef5867aad3d2e9730a8fbcdaca4
-
-      // generate the grid
-
-      return $this->parser->parse('_grid', $grid, true);
-
     }
 
     public function generateCategories()
